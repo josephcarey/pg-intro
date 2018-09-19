1bc3a51bb38c02086000776cbc990c8a1383a3e0
Joseph Carey <josephcareycomposer@gmail.com>
2018-09-19 15:52:05 -0500

    add a link to our postgres db via pg

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// const { Pool, Client } = require( 'pg' );
+
+const pg = require( 'pg' );
+const Pool = pg.Pool;
+
+const pool = new Pool( {
+    database: 'shoe_store', // name of database
+    host: 'localhost', // where it is
+    port: 5432, // port for your database, 5432 is default for postgres
+    max: 10, // how many simultaneous connections
+    idleTimeoutMillis: 30000 // timeout time
+} )
+
+// pool.on( 'connect', () => {
+//     console.log( '### Postgresql connected' );
+// } )
+// pool.on( 'error', ( error ) => {
+//     console.log( '### Error with postgres pool:', error );
+// } )
+
+pool.connect( ( err ) => {
+    if ( err ) {
+        console.log( 'fuuuck man', err );
+    } else {
+        console.log( 'up and running!' );
+    }
+} );
+
+pool.query( `SELECT * FROM "shoes";` )
+    .then( ( results ) => {
+        console.log( '### Back from db with:' );
+        console.log( results.rows );
+    } ).catch( ( error ) => {
+        console.log( '### Error with SQL select for shoes:', error );
+    } );
